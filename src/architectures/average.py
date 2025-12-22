7f59a4f81368354245178f996545791a7b20574c
Dan Vancea <100865899+TheSpaceHub@users.noreply.github.com>
2025-12-22 16:07:46 +0100

    Developed upscaling model

--- src/architectures/average.py
+++ src/architectures/average.py
@@ -1,3 +1,50 @@
-class Average():
-    def __init__(self):
-        pass
+import numpy as np
+import keras
+import tensorflow as tf
+from keras import ops
+from keras import layers
+
+@keras.saving.register_keras_serializable()
+class Upscaler(layers.Layer):
+    """Upscales images by superposing grids and averaging colors."""
+
+    def __init__(self, up_ratio: float, name="upscaler", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.up_ratio = up_ratio
+
+    def call(self, inputs):
+        shape = tf.shape(inputs)
+        height = shape[1]
+        width = shape[2]
+
+        # We round the new width and height casting twice (tf is weird)
+        new_height = tf.cast(height, tf.float32) * self.up_ratio
+        new_width = tf.cast(width, tf.float32) * self.up_ratio
+        
+        new_height = tf.cast(new_height, tf.int32)
+        new_width = tf.cast(new_width, tf.int32)
+
+        # Resize
+        return tf.image.resize(inputs, [new_height, new_width], method='area')
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"up_ratio": self.up_ratio})
+        return config
+
+@keras.saving.register_keras_serializable()
+class Average(keras.Model):
+    """Defines a model which upscales images by averaging. Training does not modify its behavior"""
+
+    def __init__(self, up_ratio=2, name="average", **kwargs):
+        super().__init__(name=name, **kwargs)
+        self.up_ratio = up_ratio
+        self.upscaler = Upscaler(up_ratio)
+
+    def call(self, inputs):
+        return self.upscaler(inputs)
+
+    def get_config(self):
+        config = super().get_config()
+        config.update({"up_ratio": self.up_ratio})
+        return config
--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
 import architectures as archs
+import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 def main():
     print("Main loop running")
-    avg = archs.Average()
+    avg = archs.Average(up_ratio=4.5)
+
+    avg.build((None, None, None, 3))
+    avg.compile(
+        optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+
+    img_small = np.random.rand(1, 400, 640, 3).astype("float32")
+
+    out_small = avg(img_small)
+
+    plt.imshow(img_small[0])
+    plt.axis('off')
+    plt.show()
+    plt.imshow(out_small.numpy()[0])
+    plt.axis('off')
+    plt.show()
     
+
+
 if __name__ == "__main__":
     main()
